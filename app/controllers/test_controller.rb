da7b39d2beed0efaa35b39a7ee1602b33e61bef6
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2019-12-20 19:03:35 +0900

    testpage add

---
+++ app/controllers/test_controller.rb
@@ -0,0 +1,6 @@
+#　テスト用に適当に作成　削除してください
+
+class TestController < ActionController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ビュー確認用に適当に作成。手で作っているので削除してください。
+  root 'test#index' 
 end
