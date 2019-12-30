befc215eae2f56d84fbf0cac50745f6b9afe963d
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2019-12-30 19:16:13 +0900

    sell_views fn

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#index'
+  root 'items#index'
 resources :items, except: :show
 end
