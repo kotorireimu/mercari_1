fcc42002af9fea8dc59326fa05b9a034ba34273d
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2019-12-30 12:00:24 +0900

    UP_views up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/index'
   get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "items#index"
+  root "users#index"
 end
