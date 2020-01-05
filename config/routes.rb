f33b89c1fb14859d05ee4ba39470d623869e0260
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2020-01-05 14:21:40 +0900

    buy_item_page view fn

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'homes/edit'
   devise_for :users
   get 'items/index'
+  get 'buy/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#edit'
+  root 'buy#index'
   
   resources :items, except: :show
   resources :homes, except: :show
