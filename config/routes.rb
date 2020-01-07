4fa465f93162c94296b552da6307cbe95a7fc301
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2020-01-07 21:36:44 +0900

    system apdate

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'toppage/index'
-  get 'homes/index'
+  # get 'toppage/index'
+  # get 'homes/index'
   get 'homes/destroy'
   get 'homes/edit'
   get 'homes/identification'
   devise_for :users
-  get 'items/index'
+  # get 'items/index'
   get 'buy/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'toppage#index'
   resources :items, except: :show
   resources :homes, except: :show
-  resources :toppage, except: :show
+  # resources :toppage, except: :show
 end
