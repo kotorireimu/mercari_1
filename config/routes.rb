19cdc10d03581c068cf3be39e9e0d2eb89ef6e2e
qsdcv <usausapokepoke0576@gmail.com>
2020-01-08 21:15:49 +0900

    prodakutdetails_create
    商品詳細ページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   get 'homes/index'
   get 'homes/destroy'
-  get 'homes/edit'
+  get 'homes/edit' 
+  get 'details/index'
   devise_for :users
   get 'items/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'homes#edit'
+  root 'details#index'
   
   resources :items, except: :show
   resources :homes, except: :show
