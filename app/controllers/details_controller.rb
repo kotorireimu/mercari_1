7af3df51c855dd83ab4ccfdcf24c3e65bd23bd3c
qsdcv <usausapokepoke0576@gmail.com>
2020-01-17 14:56:27 +0900

    edit

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -2,7 +2,7 @@ class DetailsController < ApplicationController
   
   def index
     @item = Item.find(params[:item_id])
-    @item_image = ItemImage.find_by(params[:item_id])
+    @item_image = ItemImage.find(params[:item_id])
     @user = User.find(params[:user_id])
   end
   
--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,7 @@
 class ImagesController < ApplicationController
   def show_image
     @item = Item.find(params[:id])
-    @image = @item.item_images.find_by(params[:id])
+    @image = @item.item_images.first(params[:id])
     send_data @image.image_url, :type => 'image/svg', :disposition => 'inline'
   end
 end
