c82baae328f7f78135e679617bbdb0435e1cf645
@Junk8383 <sksk14@icloud.com>
2020-01-17 11:01:01 +0900

    rewrite

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -22,7 +22,6 @@ class HomesController < ApplicationController
   end
 
   def card
-    @user = User.find(current_user.id)
     card = Card.find_by(user_id: current_user.id)
     Payjp.api_key =  'sk_test_274c8b939aa04632fc0cb6dd'
     customer = Payjp::Customer.retrieve(card.customer_id)
