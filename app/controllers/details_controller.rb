659a6a2aba88c3bb8abf464565eadbd44347ba70
kotorireimu <yamikoukiyuu@yahoo.co.jp>
2020-01-17 20:49:15 +0900

    update

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,7 +3,7 @@ class DetailsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @item_image = ItemImage.find(params[:id])
-    # @user = User.find(params[:user_id])
+    
   end
   
 
