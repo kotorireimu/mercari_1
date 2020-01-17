a4e76996d4084e9314d1be1bde02fbd490e3200f
qsdcv <usausapokepoke0576@gmail.com>
2020-01-17 14:25:53 +0900

    edit_2

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,7 +3,7 @@ class DetailsController < ApplicationController
   def index
     @item = Item.find_by(params[:item_id])
     @item_image = ItemImage.find_by(params[:item_id])
-    @user = User.find_by(params[:user_id])
+    @user = User.find(params[:user_id])
   end
   
 
