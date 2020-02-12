908d00ba65c4c246b954649e3d909f6e96523064
@Junk8383 <sksk14@icloud.com>
2020-02-12 21:25:08 +0900

    edit

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -3,7 +3,11 @@ class DetailsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @item_image = ItemImage.find_by(item_id: @item.id)
-
+    if @item.user_id == current_user.id
+      redirect_to item_path(@item)
+    else
+      render :show
+    end
   end
   
 end
